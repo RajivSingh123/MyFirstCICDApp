3e5627fbc9b508d11904829728e76db1e5f107eb
RajivSingh123 <innovaterajiv@gmail.com>
2025-04-22 19:19:24 +0530

    Second commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MYcicdApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
